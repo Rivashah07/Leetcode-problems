ca34f5fad1bfed619e7173115fb77bfe7652ca60
Riva Shah <rivashah795@gmail.com>
2026-06-08 16:49:32 +0530

    quick sort

---
+++ Quick sort.py
@@ -0,0 +1,16 @@
+#2161 partition-array-according-to-given-pivot
+class Solution:
+    def pivotArray(self, nums, pivot):
+        less = []
+        equal = []
+        greater = []
+
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num > pivot:
+                greater.append(num)
+            else:
+                equal.append(num)
+
+        return less + equal + greater
