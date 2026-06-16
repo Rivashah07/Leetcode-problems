905e991ccbfe2b9e3ec18161ccb3e9fcc2f34e71
Riva Shah <rivashah795@gmail.com>
2026-06-16 10:39:26 +0530

    special operator

---
+++ string with special operator.py
@@ -0,0 +1,17 @@
+# 3612 string with special operator
+class Solution(object):
+    def processStr(self, s):
+        result = ""
+
+        for ch in s:
+            if 'a' <= ch <= 'z':
+                result += ch
+            elif ch == '*':
+                if result:
+                    result = result[:-1]
+            elif ch == '#':
+                result += result
+            elif ch == '%':
+                result = result[::-1]
+
+        return result
