78154629b444cc34b89caf05d830de4bf473a698
Riva Shah <rivashah795@gmail.com>
2026-06-22 19:49:17 +0530

    max no of balloons

---
+++ max no of balloons.py
@@ -0,0 +1,13 @@
+#1189 maximum number of balloons
+from collections import Counter
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        c = Counter(text)
+
+        return min(
+            c['b'],
+            c['a'],
+            c['l'] // 2,
+            c['o'] // 2,
+            c['n']
+        )
