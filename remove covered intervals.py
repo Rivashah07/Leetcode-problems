0cf3981772b56e964066e26dd856f242284903a7
Riva Shah <rivashah795@gmail.com>
2026-07-06 21:53:36 +0530

    remove covered intervals

---
+++ remove covered intervals.py
@@ -0,0 +1,14 @@
+#1288 REmove Covered Intervals
+class Solution:
+    def removeCoveredIntervals(self, intervals):
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+
+        count = 0
+        max_end = -1
+
+        for start, end in intervals:
+            if end > max_end:
+                count += 1
+                max_end = end
+
+        return count
