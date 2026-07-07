4c4334e98aa5140bf02651b252c40dcdc19f3f6c
Riva Shah <rivashah795@gmail.com>
2026-07-07 11:27:15 +0530

    conconate non zero digits and mulitiply by sum

---
+++ concatenate non zero digits.py
@@ -0,0 +1,13 @@
+#3754 Concatenate Non-Zero Digits and Multiply by Sum of Digits
+class Solution(object):
+    def sumAndMultiply(self, n):
+        x = 0
+        digit_sum = 0
+
+        for ch in str(n):
+            if ch != '0':
+                d = int(ch)
+                x = x * 10 + d
+                digit_sum += d
+
+        return x * digit_sum
