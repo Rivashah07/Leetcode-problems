b5442b2572749f1c3f61f77f7143c73d7f5d71db
Riva Shah <rivashah795@gmail.com>
2026-07-16 20:08:25 +0530

    Sum of GCD of formed pairs

---
+++ sum of GCD of pairs.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def gcdSum(self, nums):
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        prefixGcd = []
+        mx = 0
+
+        for x in nums:
+            if x > mx:
+                mx = x
+            prefixGcd.append(gcd(x, mx))
+
+        prefixGcd.sort()
+
+        ans = 0
+        i, j = 0, len(prefixGcd) - 1
+
+        while i < j:
+            ans += gcd(prefixGcd[i], prefixGcd[j])
+            i += 1
+            j -= 1
+
+        return ans
