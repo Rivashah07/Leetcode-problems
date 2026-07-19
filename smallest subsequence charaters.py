ff80642d49beb077be1189ce25bb59cc57987676
Riva Shah <rivashah795@gmail.com>
2026-07-19 18:45:43 +0530

    smallest subsequence of distinct characters

---
+++ smallest subsequence charaters.py
@@ -0,0 +1,22 @@
+#1081 smallest subsequence of distinct characters
+class Solution(object):
+    def smallestSubsequence(self, s):
+        last = {}
+
+        for i, ch in enumerate(s):
+            last[ch] = i
+
+        stack = []
+        used = set()
+
+        for i, ch in enumerate(s):
+            if ch in used:
+                continue
+
+            while stack and stack[-1] > ch and last[stack[-1]] > i:
+                used.remove(stack.pop())
+
+            stack.append(ch)
+            used.add(ch)
+
+        return "".join(stack)
