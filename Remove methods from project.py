89595b4ec8fb755d7cea44b18c163c9459e6e060
Riva Shah <rivashah795@gmail.com>
2026-08-05 09:53:15 +0530

    Remove methods from project

---
+++ Remove methods from project.py
@@ -0,0 +1,23 @@
+#3310 Remove methods from project
+class Solution(object):
+    def remainingMethods(self, n, k, invocations):
+        graph = [[] for _ in range(n)]
+        for u, v in invocations:
+            graph[u].append(v)
+
+        suspicious = [False] * n
+        stack = [k]
+        suspicious[k] = True
+
+        while stack:
+            u = stack.pop()
+            for v in graph[u]:
+                if not suspicious[v]:
+                    suspicious[v] = True
+                    stack.append(v)
+
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        return [i for i in range(n) if not suspicious[i]]
