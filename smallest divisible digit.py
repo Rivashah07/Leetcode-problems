80db9e2212db8dc5ced38e08ee634eda3661fcc2
Riva Shah <rivashah795@gmail.com>
2026-08-06 19:59:25 +0530

    smallest divisble digit product 1

---
+++ smallest divisible digit.py
@@ -0,0 +1,12 @@
+#3345 Smallest Divisible Digit product 1
+class Solution:
+    def smallestNumber(self, n, t):
+        while True:
+            p=n
+            pr=1
+            while p>0:
+                pr*=p%10
+                p//=10
+            if pr%t==0:
+                return n
+            n+=1
