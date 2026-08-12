595a5cab2e7647826e551372b656767231c18695
Riva Shah <rivashah795@gmail.com>
2026-08-12 10:33:42 +0530

    length of longest subarray with max frequency

---
+++ length of subarray with k freq.py
@@ -0,0 +1,13 @@
+#2958 Length of Longest Subarray With Maximum Frequency
+class Solution:
+    def maxSubarrayLength(self, nums, k):
+        freq = defaultdict(int)
+        left = 0
+        ans = 0
+        for right in range(len(nums)):
+            freq[nums[right]] += 1
+            while freq[nums[right]] > k:
+                freq[nums[left]] -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
