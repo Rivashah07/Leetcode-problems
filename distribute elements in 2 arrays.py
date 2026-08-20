cc55d73d71260615c70555c15ae6fcd3d5cabfc0
Riva Shah <rivashah795@gmail.com>
2026-08-20 15:31:39 +0530

    distribute elements into 2 arrays

---
+++ distribute elements in 2 arrays.py
@@ -0,0 +1,7 @@
+#3069 Distribute Elements in Two Arrays
+class Solution(object):
+    def resultArray(self, nums):
+        a, b = [nums[0]], [nums[1]]
+        for x in nums[2:]:
+            (a if a[-1] > b[-1] else b).append(x)
+        return a + b
